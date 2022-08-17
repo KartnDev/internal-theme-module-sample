fb4d85fd86a79432f1c86397a177d1d645c210f1
kartondev <d.cherkasov@haulmont.com>
2022-08-17 20:13:45 +0300

    init commit

---
+++ app/src/main/java/com/company/internalthememodulesample/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.company.internalthememodulesample.screen.user;
+
+import com.company.internalthememodulesample.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
